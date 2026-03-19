6602adee4c47c5b1c450962020f8f369f8805346
Biển Xanh <84816418+thesea58@users.noreply.github.com>
2026-03-19 21:31:25 +0700

    [update] chỉnh đường dẫn ảnh

--- app/views/client/Trang-chu.php
+++ app/views/client/Trang-chu.php
@@ -20,7 +20,7 @@
       <div class="container-fluid">
 
         <a class="navbar-brand d-flex align-items-center" href="trang-chu.php">
-          <img src="img/Anh/Banner/logo.jpg" alt="Logo" class="nav-logo" style="height: 120px;">
+          <img src="app/views/client/img/Anh/Banner/logo.jpg" alt="Logo" class="nav-logo" style="height: 120px;">
           <span class="brand-text ms-2" style="color: #8B4513; font-weight: 800; font-size: 1.4rem; text-transform: uppercase;">ĐẶC SẢN BA MIỀN</span>
         </a>
 
@@ -74,16 +74,16 @@
             <div id="bannerSlider" class="carousel slide shadow rounded overflow-hidden my-5" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img src="img/Anh/Banner/banner.jpg" class="d-block w-100 banner-img" alt="Banner 1">
+          <img src="app/views/client/img/Anh/Banner/banner.jpg" class="d-block w-100 banner-img" alt="Banner 1">
         </div>
         <div class="carousel-item">
-          <img src="img/Anh/Trung/nemchua_thanhhoa.jpg" class="d-block w-100 banner-img" alt="Banner 2">
+          <img src="app/views/client/img/Anh/Trung/nemchua_thanhhoa.jpg" class="d-block w-100 banner-img" alt="Banner 2">
         </div>
         <div class="carousel-item">
-          <img src="img/Anh/Nam/banhpia_soctrang.jpg" class="d-block w-100 banner-img" alt="Banner 3">
+          <img src="app/views/client/img/Anh/Nam/banhpia_soctrang.jpg" class="d-block w-100 banner-img" alt="Banner 3">
         </div>
         <div class="carousel-item">
-          <img src="img/Anh/Bac/banhdauxanh_haiduong.png" class="d-block w-100 banner-img" alt="Banner 4">
+          <img src="app/views/client/img/Anh/Bac/banhdauxanh_haiduong.png" class="d-block w-100 banner-img" alt="Banner 4">
         </div>
       </div>
   
@@ -103,7 +103,7 @@
         
         <div class="col-md-3 col-sm-6 mb-4">
           <div class="card h-100 border-0 shadow-sm product-card">
-            <img src="img/Anh/Trung/tre_binhdinh.jpg" class="card-img" alt="Tré Bình Định">
+            <img src="app/views/client/img/Anh/Trung/tre_binhdinh.jpg" class="card-img" alt="Tré Bình Định">
             <div class="card-body text-center">
               <h5 class="card-title fw-bold text-brown">Tré Bình Định</h5>
               <p class="card-text text-danger fw-bold">130.000đ</p>
@@ -114,7 +114,7 @@
 
         <div class="col-md-3 col-sm-6 mb-4">
           <div class="card h-100 border-0 shadow-sm product-card">
-            <img src="img/Anh/Nam/banhpia_soctrang.jpg" class="card-img" alt="Bánh Pía Sóc Trăng">
+            <img src="app/views/client/img/Anh/Nam/banhpia_soctrang.jpg" class="card-img" alt="Bánh Pía Sóc Trăng">
             <div class="card-body text-center">
               <h5 class="card-title fw-bold text-brown">Bánh Pía Sóc Trăng</h5>
               <p class="card-text text-danger fw-bold">90.000đ</p>
@@ -125,7 +125,7 @@
 
         <div class="col-md-3 col-sm-6 mb-4">
           <div class="card h-100 border-0 shadow-sm product-card">
-            <img src="img/Anh/Bac/traugacbep_TayBac.png" class="card-img" alt="Thịt Trâu Gác Bếp">
+            <img src="app/views/client/img/Anh/Bac/traugacbep_TayBac.png" class="card-img" alt="Thịt Trâu Gác Bếp">
             <div class="card-body text-center">
               <h5 class="card-title fw-bold text-brown">Thịt Trâu Gác Bếp</h5>
               <p class="card-text text-danger fw-bold">500.000đ</p>
@@ -136,7 +136,7 @@
 
         <div class="col-md-3 col-sm-6 mb-4">
           <div class="card h-100 border-0 shadow-sm product-card">
-            <img src="img/Anh/Trung/yenxao_khanhhoa.jpg" class="card-img" alt="Yến Xào Khánh Hòa">
+            <img src="app/views/client/img/Anh/Trung/yenxao_khanhhoa.jpg" class="card-img" alt="Yến Xào Khánh Hòa">
             <div class="card-body text-center">
               <h5 class="card-title fw-bold text-brown">Yến Xào Khách Hòa</h5>
               <p class="card-text text-danger fw-bold">500.000đ</p>
@@ -152,7 +152,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-4 mb-3 text-brown">
-            <img src="img/Anh/Banner/logo.jpg" alt="Logo" class="nav-logo" style="height: 120px;">
+            <img src="app/views/client/img/Anh/Banner/logo.jpg" alt="Logo" class="nav-logo" style="height: 120px;">
             <p class="mt-3">
               Đặc sản ba miền – Tinh hoa ẩm thực Việt!<br>
               Mang đến hương vị truyền thống chuẩn vị Bắc – Trung – Nam.
